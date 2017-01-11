74782a827f9bfff34e2111b92504a4a0a4821db2
jkbits1 <jkbits1@users.noreply.github.com>
2017-01-11 01:19:11 +0000

    initial state copied from original elmD3 folder

---
+++ elm18/elmD3/displayWheel2.js
@@ -0,0 +1,233 @@
+// NOTE: all code here is based heavily on this excellent article
+// http://www.visualcinnamon.com/2015/09/placing-text-on-arcs.html
+
+"use strict";
+
+// passing multiple arrays signal to d3 to support multiple
+// circles
+function circles() {
+  return [
+      ["a", "b", "c"]
+    , ["m", "n", "o"]
+    , ["p", "q", "r"]
+    , ["x", "y", "z"]
+  ]
+}
+
+var svg;
+
+function showCircle (donutDataList) {
+
+//Create a color scale
+  var colorScale = d3.scale.linear()
+    .domain([0.03, 2.1, 6])
+    .range([
+      "#3c7bb6",
+      "#D8D865",
+      "#d7191c"])
+    .interpolate(d3.interpolateHcl);
+
+//Create an arc function
+  var arc = d3.svg.arc()
+      .innerRadius(function (d) {
+        return d.data.sz * 0.1 * width * 0.75 / 2;
+      })
+      .outerRadius(function (d) {
+        return d.data.sz * 0.1 * width * 0.75 / 2 + 30;
+      })
+    ;
+
+  var screenWidth = window.innerWidth;
+
+  var initWH = 400;
+
+  var adjRatio = 1.0;
+
+  var margin = {left: 20, top: 20, right: 20, bottom: 20},
+
+  width = Math.min(screenWidth, 500 * adjRatio) - margin.left - margin.right,
+  height = Math.min(screenWidth, 500 * adjRatio) - margin.top - margin.bottom;
+
+  svg = d3.select("#chart svg").remove();
+
+  var svgWidth  = 400; //(width + margin.left + margin.right);
+  var svgHeight = 400; // (height + margin.top + margin.bottom);
+
+  var transWidth  = 200; // ((width / 2 + margin.left) - 50)
+  var transHeight = 175; // ((height / 2 + margin.top) - 50)
+
+  svg = d3.select("#chart").append("svg")
+  .attr("width", svgWidth )
+  .attr("height", svgHeight)
+  .append("g").attr("class", "wrapper")
+  .attr("transform",
+    "translate(" +
+     transWidth + "," +
+      transHeight + ")")
+  ;
+
+  svg = svg.selectAll("g")
+  .data(circles)
+  .enter().append("g")
+  .attr("class", "circles")
+  ;
+
+//Turn the pie chart 90 degrees counter clockwise, so it starts at the left
+  var pie = d3.layout.pie()
+    .startAngle(-90 * Math.PI / 180)
+    .endAngle(-90 * Math.PI / 180 + 2 * Math.PI)
+    .value(
+      d => {
+        return d.value;
+      }
+    )
+    .padAngle(.01)
+    .sort(null);
+
+  function getDataList (d) {
+    var n = 3;
+
+    if (d[0] == "a") {
+      n = 0;
+    }
+    else if (d[0] == "m") {
+      n = 1;
+    }
+    else if (d[0] == "p") {
+      n = 2;
+    }
+
+    return pie(donutDataList[n]);
+  }
+
+  donuts();
+
+  function donuts ()
+  {
+  //Create the donut slices
+
+    svg.selectAll(".donutArcSlices")
+    .data(getDataList)
+    .enter().append("path")
+    .attr("class", "donutArcSlices")
+
+    .attr("d", arc)
+    .style("fill", (d, i) => {
+      if (i === 7) return "#CCCCCC"; //Other
+      else return colorScale(i);
+    })
+
+    .each(function (d, i) {
+
+      //A regular expression that captures all in between the start of a string (denoted by ^) and a capital letter L
+      //The letter L denotes the start of a line segment
+      //The "all in between" is denoted by the .+?
+      //where the . is a regular expression for "match any single character except the newline character"
+      //the + means "match the preceding expression 1 or more times" (thus any single character 1 or more times)
+      //the ? has to be added to make sure that it stops at the first L it finds, not the last L
+      //It thus makes sure that the idea of ^.*L matches the fewest possible characters
+      //For more information on regular expressions see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+      var firstArcSection = /(^.+?)L/;
+
+      //Grab everything up to the first Line statement
+      //The [1] gives back the expression between the () (thus not the L as well) which is exactly the arc statement
+      var newArc;
+      var firstArcArray = firstArcSection.exec(d3.select(this).attr("d"));
+
+      if (firstArcArray == null) {
+        newArc = "";
+      }
+      else {
+        newArc = firstArcArray[1];
+      }
+      //Replace all the comma's so that IE can handle it -_-
+      //The g after the / is a modifier that "find all matches rather than stopping after the first match"
+      newArc = newArc.replace(/,/g, " ");
+
+      // flips the text
+      //If the end angle lies beyond a quarter of a circle (90 degrees or pi/2)
+      //flip the end and start position
+      if (d.endAngle > 90 * Math.PI / 180) {
+        var startLoc = /M(.*?)A/,		//Everything between the first capital M and first capital A
+          middleLoc = /A(.*?)0 0 1/,	//Everything between the first capital A and 0 0 1
+          endLoc = /0 0 1 (.*?)$/;	//Everything between the first 0 0 1 and the end of the string (denoted by $)
+
+        //Build up the new arc notation, set the sweep-flag to 0
+        if (firstArcArray == null) {
+            newArc = "";
+        }
+        else {
+            //Flip the direction of the arc by switching the start en end point (and sweep flag)
+            //of those elements that are below the horizontal line
+            var newStart = endLoc.exec(newArc)[1];
+            var newEnd = startLoc.exec(newArc)[1];
+            var middleSec = middleLoc.exec(newArc)[1];
+
+            newArc = "M" + newStart + "A" + middleSec + "0 0 0 " + newEnd;
+        }
+      }
+
+      //Create a new invisible arc that the text can flow along
+      svg.append("path")
+      .attr("class", "hiddenDonutArcs")
+      .attr("id", "donutArc" + i)
+      .attr("d", newArc)
+      .style("fill", "none");
+    });
+
+    // text, left aligned, centered, flipped (see specific sections below)
+    //Append the label names on the outside
+    svg.selectAll(".donutText")
+    .data( getDataList )
+    .enter().append("text")
+    .attr("class", "donutText")
+    //Move the labels below the arcs for those slices with an end angle greater than 90 degrees
+    .attr("dy", (d, i) => {
+      var dy = (d.endAngle > 90 * Math.PI / 180 ? 18 : -11);
+
+      var adjDy = dy;
+      var sz4adj = 28;
+      var sz2adj = 38.5;
+      var sz2adj2 = 6.8;
+
+      if (dy < 0) {
+        if (d.data.sz == 6) {
+          adjDy = (-0.3 * dy) * 17;
+        }
+        else if (d.data.sz == 4) {
+          adjDy = (-0.3 * dy) * sz4adj;
+        }
+        else if (d.data.sz == 2) {
+          adjDy = (-0.3 * dy) * sz2adj;
+        }
+        else {
+          adjDy = dy * -1.9;
+        }
+      }
+      else {
+        if (d.data.sz == 6) {
+          adjDy = (-0.9 * dy) * 2.4;
+        }
+        else if (d.data.sz == 4) {
+          adjDy = (-0.9 * dy) * 4.8;
+        }
+        else if (d.data.sz == 2) {
+          adjDy = (-0.9 * dy) * sz2adj2;
+        }
+        else {
+          adjDy = dy * -0.44;
+        }
+      }
+
+      adjDy = adjDy * adjRatio;
+
+      return adjDy;
+    })
+
+    .append("textPath")
+    .attr("startOffset", "50%")
+    .style("text-anchor", "middle")
+    .attr("xlink:href", (d, i) => "#donutArc" + i)
+    .text(d => d.data.name);
+  }
+}
---
+++ elm18/elmD3/puzzle2-ui.elm
@@ -0,0 +1,573 @@
+port module WheelApp exposing (..)
+
+import PuzzleModule exposing (..)
+import Wheel exposing (..)
+
+import Html exposing (..)
+import Html.App as HtmlApp
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (..)
+import Text exposing (color)
+import Color exposing (..)
+
+import String
+import List exposing (..)
+
+
+type alias ModelInputs  = (Int, String, String, String, String)
+type alias ModelButtons = List Bool
+
+-- values generated from UI input
+type alias ModelResults =
+  (WheelPosition, WheelLoop, WheelLoop, WheelLoop,
+    List LoopsPermutation,                      List LoopsPermutation,
+    (List (LoopsPermAnswers, LoopsPermutation), List (LoopsPermAnswers, LoopsPermutation),
+      List (LoopsAnswerLoop, LoopsPermutation), List (LoopsAnswerLoop, LoopsPermutation)
+      , (LoopsPermAnswers, LoopsPermutation)
+      ))
+
+type alias Model = (ModelList, ModelInputs, ModelButtons, ModelResults)
+
+type alias ModelList = List (ModelInputs, ModelButtons)
+
+type Msg =
+      NoOp |
+      Back |
+      Circle1Field String |
+      Circle2Field String | Circle3Field String | Circle4Field String |
+      ShowLoop2 | ShowLoop3 | ShowLoopAns |
+      ShowPerms2 | ShowPerms3 |
+      ShowAns |
+      ShowState |
+      ChangeWheel |
+      D3Response (List String) |
+      Rotate1 |
+      Rotate2 |
+      Rotate3
+
+buttonVal : List Bool -> Int -> Bool
+buttonVal list num =
+  let
+    h = head (drop (num - 1) list)
+  in
+    case h of
+      Just x  -> x
+      Nothing -> False
+
+-- max buttons is 9, 10 items in list
+maxButton = 10
+buttonListToggle list num = take (num-1) list ++ [not <| buttonVal list num] ++ take (maxButton - num) (drop num list)
+
+
+buttonClassList = classList [
+--  ("btn", True), ("btn-default", True)
+--    ("loopButton", True)
+  ]
+
+buttonClassList2 = classList [
+--  ("btn", True), ("btn-default", True)
+    ("loopButton", True)
+  ]
+
+backButton : Html Msg
+backButton    = uiButton Back       "Step Back"
+
+answersButton : Bool -> Html Msg
+answersButton hide  = showLoopButton2 ("Show Answers", "Hide Answers") hide ShowAns buttonClassList2
+
+stateButton : Bool -> Html Msg
+stateButton  hide   = showLoopButton2 ("Show State", "Hide State")     hide ShowState buttonClassList2
+
+perms2Button : Bool -> Html Msg
+perms2Button hide   = showLoopButton2 ("Show Perms 2", "Hide Perms 2") hide ShowPerms2 buttonClassList2
+
+perms3Button : Bool -> Html Msg
+perms3Button hide   = showLoopButton2 ("Show Perms 3", "Hide Perms 3") hide ShowPerms3 buttonClassList2
+
+
+uiButton : Msg -> String -> Html Msg
+uiButton action label = Html.button
+  [
+      buttonClassList
+    , Html.Events.onClick action]
+  [ Html.text label ]
+
+showLoopButton labels hide action =
+  let
+    label =
+      if hide == True then
+        snd labels
+      else
+        fst labels
+  in
+    Html.button
+      [   buttonClassList
+        , Html.Events.onClick action
+      ]
+      [ Html.text label ]
+
+showLoopButton2 labels hide action classList =
+  let
+    label =
+      if hide == True then
+        snd labels
+      else
+        fst labels
+  in
+    Html.button
+      [   classList
+        , Html.Events.onClick action
+      ]
+      [ Html.text label ]
+
+
+inputField2 : String -> String -> String ->
+              (String -> Msg) ->
+              List (String, String) -> Html Msg
+inputField2 idVal default text updateItem inputStyle =
+  input
+    [ placeholder default, Attr.value text
+    , onInput updateItem
+      , id idVal
+      , class "form-control col-sm-2 wheelStyle"
+    ]
+    []
+formGroup : String -> String -> String -> (String -> Msg) -> List (String, String) -> Msg -> Html Msg
+formGroup lbl idVal val updateItem style msg =
+  div [
+    class "wheelInput"
+  ] [
+        label [
+          for idVal
+        , classList [("control-label", True),("col-sm-4", True), ("wheelInputLabel", True)]
+        ] [
+          text <| "Wheel " ++ lbl
+        ]
+      , inputField2 idVal lbl val updateItem style
+  ]
+
+wheelOnlyRow idx wheelLabel wheelData =
+    div [
+          class "row"
+          ] [
+      div [
+        class "col-sm-2 wheelRowLabel"
+        ] [ text wheelLabel ]
+      ,
+      div [
+        class "col-sm-2 wheelRowData"
+        ] [ text <| wheelData ]
+    ]
+
+wheelRow idx wheelLabel loopLabel wheelData loopData action hide =
+    div [
+      classList [("row", True),("wheelRow", True)]
+      ] [
+
+        div [
+          class "col-sm-2 wheelRowLabel"
+        ] [ text wheelLabel ]
+      , div [
+      class "col-sm-2 wheelRowData"
+      ] [ text <| wheelData ]
+      , div [
+      class "col-sm-1 plusAdjust"
+      ] [ showLoopButton ("+", "-") hide action ]
+      , div [
+        class "col-sm-2", style <| (displayStyle hide) ++ [("font-weight", "700")]
+        ] [
+        text loopLabel
+      ]
+
+      , div [
+        class "col-sm-2 loopDataAdjust", style <| displayStyle hide
+        ] [ text loopData ]
+    ]
+
+foundAnswerIndicator : List (a,b) -> Bool -> Html Msg
+foundAnswerIndicator answerList show =
+  let
+    found = not <| length answerList == 0
+    foundString =
+      if found then
+        "Yes"
+      else
+        "No"
+  in
+    div [ class "foundAnswer", style <| (displayStyle show) ]
+    [ text <| "Does solution exist? - "
+    , span [ style <| colorStyle <| found ] [
+        text <| foundString
+      ]
+    ]
+
+
+myStyle : List (String, String)
+myStyle =
+  [
+--    ("width", "100%")
+--  , ("height", "40px")
+--  , ("padding", "10px 0")
+--  , ("font-size", "1.8em")
+--  , ("text-align", "center")
+  ]
+
+textStyle : List (String, String)
+textStyle =
+  [
+--    ("width", "100%")
+--  , ("padding", "10px 0")
+--  , ("font-size", "1.8em")
+--  , ("text-align", "left")
+  ]
+
+displayStyle : Bool -> List (String, String)
+displayStyle show =
+  case show of
+    True ->   [("display", "block")]
+    False ->  [("display", "none")]
+
+infoRow label info displayState =
+  div [
+  class "row",
+        style
+                      (displayStyle displayState)
+                      ] [
+      div [
+        class "col-sm-2 wheelRowLabel"
+        ] [ text label ]
+    , div [
+        class "col-sm-8 permsData"
+      ] [ text <| info ]
+  ]
+
+
+-- converts Signal Model to Signal Html, using non-signal view
+--main : Signal Html
+--main = viewLift
+
+main = HtmlApp.program { init = init, view = view, update = updateModel, subscriptions = subscriptions }
+
+-- subscriptions, data responses from js
+port dataProcessedItems : (List String -> msg) -> Sub msg
+
+subscriptions : Model -> Sub Msg
+--subscriptions model = Sub.none
+subscriptions model = dataProcessedItems D3Response
+
+--viewLift : Signal Html
+--viewLift = Signal.map (view updatesChnl.address) updateModelLift
+
+-- used by main, as a non-signal function, to convert a Model to Html
+view : Model -> Html Msg
+view ( stateHistory,
+        (i, s1, s2, s3, s4),
+        buttonList,
+        (firstList, secLoop, thrLoop, ansLoop, twoListPerms, threeListPerms,
+          (ansPlusList, specificAnswer, ansPermsPlusList, specificAnswerPlusList
+            , findAnswerLazy3))
+      ) =
+  div [] [
+  div [
+    class "container"
+    ]
+  [
+         div [
+         class "row title"
+         ] [
+          h2 [] [text "Elm Puzzle Calculator"]
+         ]
+
+       , div [
+          class "row"
+          ] [
+          div [
+            class "btn-group"
+            ] [
+              perms2Button  <| buttonVal buttonList 5
+            , perms3Button  <| buttonVal buttonList 6
+            , answersButton <| buttonVal buttonList 1
+          ]
+        , div [
+            classList [("btn-group", True), ("stateButton", True)]
+          ] [
+            backButton
+          , stateButton <| buttonVal buttonList 7
+        ]
+      ]
+    , br [] []
+
+    , Html.form [
+          classList [("wheelsForm", True), ("form-inline", True) ]
+        ] [
+            div [] [
+            formGroup "1" "wheel1input"     s1 Circle1Field myStyle Rotate1
+          , formGroup "2" "wheel2input"     s2 Circle2Field myStyle Rotate2
+          ]
+          , div [] [
+            formGroup "3" "wheel3input"     s3 Circle3Field myStyle Rotate3
+          , formGroup "Ans" "wheelAnsInput" s4 Circle4Field myStyle Rotate1
+          ]
+        ]
+    , div [classList [("rotBtns", True)]] [
+        rotButton "1" Rotate1
+        ,
+          rotButton "2" Rotate2
+        ,
+        rotButton "3" Rotate3
+
+      ]
+    , div [id "chart"
+      ] []
+
+    , br [] []
+
+    , div [classList [("wheelCalcs", True)]] [
+        wheelOnlyRow  1 "Wheel 1"               s1
+      , wheelRow      2 "Wheel 2"   "Loop 2"    s2 (toString secLoop) ShowLoop2   <| buttonVal buttonList 2
+      , wheelRow      3 "Wheel 3"   "Loop 3"    s3 (toString thrLoop) ShowLoop3   <| buttonVal buttonList 3
+      , wheelRow      4 "Wheel Answers" "Loop Answers"  s4 (toString ansLoop) ShowLoopAns <| buttonVal buttonList 4
+    ]
+  ]
+
+  , br [] []
+
+  , div [ classList [("answers", True)]] [
+      div [
+      class "container"
+      ]
+      [
+          div [
+          class "row"
+          ] [
+              div [
+              ] [ foundAnswerIndicator specificAnswer True
+              ]
+          ]
+
+        , div [
+          class "row"
+          ] [
+              div [
+              ] [ puzzleSolvedIndicator s1 s2 s3 s4
+              ]
+          ]
+
+        , br [] []
+        , infoRow "2 Loop Perms"  (toString twoListPerms)   <| buttonVal buttonList 5
+        , infoRow "3 Loop Perms"  (toString threeListPerms) <| buttonVal buttonList 6
+        , infoRow "answersPlus"   (toString ansPlusList )   <| buttonVal buttonList 1
+
+        , br [] []
+
+        , infoRow "findAnswers"   (toString specificAnswer )   <| buttonVal buttonList 1
+
+        , infoRow "lazyAnswer - " (toString findAnswerLazy3) <| buttonVal buttonList 1
+        , infoRow ("State change count: " ++ (toString i)) (toString stateHistory) <| buttonVal buttonList 7
+
+        , div [ style <| textStyle ++ (displayStyle False)] [ text ("answersPerms - " ++ (toString ansPermsPlusList)) ]
+        , div [ style <| textStyle ++ (displayStyle False)] [ text ("displayAnswer - " ++ (toString specificAnswerPlusList)) ]
+
+      ]
+    ]
+  ]
+
+rotButton labelNum msg =
+  div [
+    class "rotButton" -- "wheelInput"
+    ] [
+  Html.button
+      [
+        Html.Events.onClick msg
+      ]
+      [ Html.text <| "Rotate " ++ labelNum ]
+  ]
+
+
+-- outgoing port to js
+port showWheel : List (List WheelItem) -> Cmd msg
+
+-- converts Signal Update (from updatesChnl) to Signal Model, 
+-- using non-signal updateModel
+--updateModelLift : Signal Model
+--updateModelLift = Signal.foldp
+--                    updateModel
+--                    initialModelState
+--                    updatesChnl.signal
+
+-- converts Update to new Model
+updateModel : Msg -> Model -> (Model, Cmd Msg)
+updateModel update (stateHistory, (i, s1, s2, s3, s4),
+                     buttonList,
+                     --(xs, xxs2, xxs3, xxs4, s9, s10, (s11, s12, s13, s14))
+                     results
+                   ) =
+  let
+    newCount   = i + 1
+    (inputs, states) = Maybe.withDefault
+                  (initialInputs, initialStates)
+                  (head stateHistory)
+    tailHistory     = Maybe.withDefault [] (tail stateHistory)
+
+    createModel (i, s1, s2, s3, s4) buttonStates forward =
+      let
+        newHistory =
+          if forward == True then
+            (inputs, buttonStates) :: stateHistory
+          else
+            tailHistory
+
+        inputs    = (i, s1, s2, s3, s4)
+
+        first     = wheelPositionFromString s1
+        answers   = wheelPositionFromString s4
+        secLoop   = makeSecLoop s2
+        thrLoop   = makeThrLoop s3
+        ansLoop   = makeAnsLoop s4
+
+        newCalcs  = (first, secLoop, thrLoop, ansLoop,
+                      twoWheelPerms first secLoop, threeLoopPerms first secLoop thrLoop,
+                      (answersPlusPerm      first secLoop thrLoop,
+                        findSpecificAnswer  first secLoop thrLoop ansLoop,
+                        answersPermsPlusList first secLoop thrLoop,
+                        displaySpecificAnswers first secLoop thrLoop answers
+                        , findAnswerCS first secLoop thrLoop ansLoop))
+      in
+        (newHistory, inputs, buttonStates, newCalcs)
+    mdl = createModel (i,       s1, s2, s3, s4) buttonList True
+    wd1 = d3DataFromString s1
+    wd2 = d3DataFromString s2
+    wd3 = d3DataFromString s3
+    wd4 = d3DataFromString s4
+  in
+    case update of
+      NoOp        ->    (createModel (i,       s1, s2, s3, s4) buttonList True, Cmd.none)
+
+      Back        ->    (createModel inputs states False, 
+                            Cmd.none
+                            -- showWheel [ wd1, wd2, wd3, wd4  ] 
+                            )
+
+      Circle1Field s -> (createModel(newCount, s,  s2, s3, s4) buttonList True, showWheel [ d3DataFromString s, wd2, wd3, wd4  ] )
+      Circle2Field s -> (createModel(newCount, s1, s,  s3, s4) buttonList True, showWheel [ wd1, d3DataFromString s, wd3, wd4  ] )
+      Circle3Field s -> (createModel(newCount, s1, s2, s,  s4) buttonList True, showWheel [ wd1, wd2, d3DataFromString s, wd4 ] )
+      Circle4Field s -> (createModel(newCount, s1, s2, s3, s)  buttonList True, showWheel [ wd1, wd2, wd3, d3DataFromString s ] )
+
+      ShowAns     ->    (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 1) True, Cmd.none)
+      ShowLoop2   ->    (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 2) True, Cmd.none)
+      ShowLoop3   ->    (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 3) True, Cmd.none)
+
+      ShowLoopAns ->    (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 4) True, Cmd.none)
+
+      ShowPerms2 ->     (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 5) True, Cmd.none)
+      ShowPerms3 ->     (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 6) True, Cmd.none)
+
+      ShowState ->      (createModel(newCount, s1, s2, s3, s4) (buttonListToggle buttonList 7) True, Cmd.none)
+
+      ChangeWheel ->    (mdl, showWheel [ wd1, wd2, wd3, wd4  ] )
+
+      -- currently a no-op
+      D3Response rs -> (createModel (i,       s1, s2, s3, s4) buttonList True, Cmd.none)
+
+      Rotate1 -> (createModel (i,       rotateNumsString s1, s2, s3, s4) buttonList True,
+                    showWheel [ d3DataFromString <| rotateNumsString s1, wd2, wd3, wd4 ])
+      Rotate2 -> (createModel (i,       s1, rotateNumsString s2, s3, s4) buttonList True,
+                    showWheel [ wd1, d3DataFromString <| rotateNumsString s2, wd3, wd4 ])
+      Rotate3 -> (createModel (i,       s1, s2, rotateNumsString s3, s4) buttonList True,
+                    --Cmd.none
+                    showWheel [ wd1, wd2, d3DataFromString <| rotateNumsString s3, wd4 ]
+                    )
+
+
+initialInputs = (0, "1,2,3", "4,5,6", "7,8,9", "12,15,18")
+initialStates = [False, False, False, False, False, False, False, False, False, False]
+initialCalcs  =
+    ([1,2,3], [[4,5,6]], [[7,8,9]], [[12,15,18]], [[[2]]], [[[3]]],
+      ([([1], [[1]])], [([1], [[1]])], [([[1]], [[1]])], [([[1]], [[1]])]
+      ,([1], [[1]])
+      )
+    )
+
+initialModelState =
+  ([],
+      initialInputs,
+    initialStates,
+    initialCalcs
+  )
+
+init =
+  let
+    mdl = initialModelState
+    wd1 = resultsToD3Data <| wheelData mdl input1
+    wd2 = resultsToD3Data <| wheelData mdl input2
+    wd3 = resultsToD3Data <| wheelData mdl input3
+    wd4 = resultsToD3Data <| wheelData mdl input4
+  in
+    (mdl, showWheel [ wd1, wd2, wd3, wd4 ] )
+
+
+input1 : ModelInputs -> String
+input1 (_, i1, _, _, _) = i1
+input2 : ModelInputs -> String
+input2 (_, _, i2, _, _) = i2
+input3 : ModelInputs -> String
+input3 (_, _, _, i3, _) = i3
+input4 : ModelInputs -> String
+input4 (_, _, _, _, i4) = i4
+
+modelInputs : Model -> ModelInputs
+modelInputs
+      ( stateHistory,
+        inputs,
+        buttonList,
+        results
+      ) = inputs
+
+wheelData : Model -> (ModelInputs -> String ) -> List Int
+wheelData model inp = wheelPositionFromString <| inp <| modelInputs model
+
+resultsToD3Data : List Int -> List { name: String }
+resultsToD3Data xs = List.map (\x -> { name = (toString x) }) xs
+
+d3DataFromString : String -> List { name: String }
+d3DataFromString = (\s -> resultsToD3Data <| wheelPositionFromString s)
+
+wheelStringFromInt : List Int -> String
+wheelStringFromInt xs = String.join ", " <| List.map (\x -> toString x) xs
+
+rotateNumsString s = wheelStringFromInt <| turnWheel (wheelPositionFromString s) 1
+
+currentAnswers s1 s2 s3 s4 =
+             (List.map sumColumn <| PuzzleModule.zip3
+               (wheelPositionFromString s1)
+               (wheelPositionFromString s2)
+               (wheelPositionFromString s3)
+             )
+
+puzzleSolved : String -> String -> String -> String -> Bool
+puzzleSolved s1 s2 s3 s4 =
+  currentAnswers s1 s2 s3 s4 == (wheelPositionFromString s4)
+
+puzzleSolvedIndicator : String -> String -> String -> String -> Html Msg
+puzzleSolvedIndicator s1 s2 s3 s4 =
+  let
+    solved = (puzzleSolved s1 s2 s3 s4)
+    solvedString =
+      if solved then
+        "Yes"
+      else
+        "No " ++ (toString <| currentAnswers s1 s2 s3 s4)
+  in
+    div [ class "solvedPuzzle" ] [
+      text <| "Puzzle solved? - "
+    , span [ style <| colorStyle <| solved ] [
+        text <| solvedString
+      ]
+    ]
+
+colorStyle : Bool -> List (String, String)
+colorStyle success =
+  case success of
+    True ->   [("color", "green")]
+    False ->  [("color", "red")]
+
